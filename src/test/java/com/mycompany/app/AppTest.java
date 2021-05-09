0035a5e65a8c825b7508dbdf4f425576743c5258
blahjunky <hungvietle@hotmail.com>
2021-05-09 13:26:10 -0700

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,37 +1,51 @@
 package com.mycompany.app;
 
+
+/** junit 4
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
+**/
 
-/**
+/** junit 5 **/
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
-*/
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
 {
     private App testApp;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        testApp = new App();
+    }
+
+
     /**
      * Rigorous Test :-)
      */
-    
-    
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
-    
+
+    @DisplayName("Simple multiplication")
     public void testMultiply() {
-        assertEquals(20, testApp.multiply(4,5));          
+        testApp = new App();
+        assertEquals(20, testApp.multiply(4,5));
     }
-    
+
+
+    @RepeatedTest(5)
+    @DisplayName("Ensure correct handling of zero")
+    public void testMultiplyWithZero() {
+        testApp = new App();
+        assertEquals(0, testApp.multiply(0,5));
+        assertEquals(0, testApp.multiply(5,0));
+    }
+
 }
